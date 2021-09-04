49ab9469f3e124110b8de35978f7494ffbc631c2
William Torres <wiltorc_182@hotmail.com>
2021-09-04 09:19:34 -0500

    Update variables.dart

---
+++ 1.variables/variables.dart
@@ -0,0 +1,21 @@
+void main() {
+
+    var name = "Willian";
+    String fullName;
+    fullName = name + " Torres";
+
+    var age = 8;
+    int secondAge = 4;
+
+    //null Safety
+    int? nullvariable = null;
+    String? nullvariable2 = null;
+
+    //final - constante
+    // final String = "Hola";
+    // const String = "Hola,";
+
+
+    print(fullName.toUpperCase());
+    print(name);
+}
