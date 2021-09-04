a59316518ebb8bd84adf3b4387dc313fd6c60774
William Torres <wiltorc2430@gmail.com>
2021-09-04 09:20:07 -0500

    Update tipos.dart

---
+++ 2.tipos/tipos.dart
@@ -0,0 +1,32 @@
+void main() {
+
+    // Number Types
+    int ageInt = 4;
+    double pointsDouble = 18.2;
+
+    num ageNum = 4;
+    num pointsNum = 22.2;
+
+    // String Types
+    var text1 = 'This is a text'; //simpleQuote
+    var text2 = "This is a number"; //DoubleQuote
+    var text3 = "This it's is a string"; //SimpleQuote
+    var text4 = 'This it\'s is a string'; //SimpleQuote
+
+    var text5 = 'This text is' + ' concatenate'; //DoubleQuote
+    var text6 = 'This it\'s is a' ' string'; //String
+
+    var text7 = "sum 3+3 is ${3+3}"; //String
+    var text8 = "Interpolate text, $text2";
+
+
+    print(text1);
+    print(text2);
+    print(text3);
+    print(text4);
+    print(text5);
+    print(text6);
+    print(text7);
+    print(text8);
+
+}
